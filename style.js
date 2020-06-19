e5233e18b8c90206cf570c4a5d90c46c20bfeb6f
lakshmikorukanti <luckyrao4e8@gmail.com>
2020-06-19 12:06:44 +0530

    getting card details

--- style.js
+++ style.js
@@ -35,13 +35,13 @@ function getDetails(){
       var location=data.restaurants[i].restaurant.location.address
       var phone=data.restaurants[i].restaurant.phone_numbers
       //console.log(name)
-      display(name,location,phone)
+      display(name,location,phone,i)
   }
   
     }
 
 }
-function display(name,location,phone){
+function display(name,location,phone,i){
     var profile=document.getElementById("profile")
     var res=document.createElement("div")
     var h4=document.createElement("h4")
@@ -58,12 +58,79 @@ function display(name,location,phone){
 res.setAttribute("class","box")
  
     profile.appendChild(res)
-    renderDom(button)
+    renderDom(button,i)
 }
-function renderDom(button){
+function renderDom(button,i){
     button.addEventListener("click",turnNextPage)
     function turnNextPage(){
-        window.location.assign("./res.html")
+      
+        window.localStorage.setItem("count", i);
+       window.location.assign("./res.html")
     }
 
+}
+var cart=document.getElementById("cart")
+var add=document.getElementById("add")
+add.addEventListener("click",addAddress)
+function addAddress(){
+   var display_address=document.getElementById("display_address")
+    var house_no=document.getElementById("house_no").value
+    var mark=document.getElementById("mark").value
+    var street=document.getElementById("street").value
+    var city=document.getElementById("city").value
+    var state=document.getElementById("state").value
+    var displayAddress=document.createElement("h4")
+    displayAddress.textContent=house_no+"\n"+mark+"\n"+street+"\n"+city+"\n"+state
+    display_address.append(displayAddress)
+}
+
+
+var add=document.getElementById("cartitems")
+add.addEventListener("click",addcartitem)
+function addcartitem(){ 
+    var sum=0
+    var array=(window.localStorage.getItem("items"))  
+    array=array.split(",")
+    var quantity=window.localStorage.getItem("quantity")
+    var quantity=quantity.split(",")
+    var cost=window.localStorage.getItem("cost")
+    var cost=cost.split(",")
+    for(var i=0;i<array.length;i++)
+    {
+        var ul=document.createElement("ul")
+        var item=document.createElement("li")
+        item.textContent="name:"+array[i]
+        var displayquantity=document.createElement("li")
+        displayquantity.textContent="quantity:"+quantity[i]
+        var displaycost=document.createElement("li")
+        var totalcost=Number(quantity[i])*Number(cost[i])
+        displaycost.textContent="cost:"+Number(quantity[i])*Number(cost[i])
+        sum+=totalcost
+        console.log(sum)
+        ul.setAttribute("class","list-group")
+        ul.setAttribute("class","displayul")
+        ul.setAttribute("class","card")
+        item.setAttribute("class","list-group-item")
+        displayquantity.setAttribute("class","list-group-item")
+      displaycost.setAttribute("class","list-group-item")
+    
+        ul.append(item,displayquantity,displaycost)
+       
+        cart.append(ul)
+
+    }
+    var totalbutton=document.createElement("button")
+    totalbutton.textContent="total bill"
+cart.append(totalbutton)
+ display_total(totalbutton,sum)
+  
+
+}
+function display_total(totalbutton,value){
+totalbutton.addEventListener("click",getTotalBill)
+function getTotalBill(){
+    var h1=document.createElement("h1")
+    h1.textContent=value
+    cart.append(h1)
+}
 }
